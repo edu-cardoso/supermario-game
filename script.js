b9ab3869c54b060383f8b6b01c597a4e6c9d5e22
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2022-11-21 16:17:59 -0300

    animação finalizada

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -8,4 +9,25 @@ const jump = () => {
     }, 500)
 }
 
-document.addEventListener('keydown', jump);
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = 'img/game-over.png';
+        mario.style.width = '80px';   
+        mario.style.marginLeft = '50px';   
+    }
+
+}, 10)
+
+
+document.addEventListener('keydown', jump); 
